b2fcf8475d5129758b3fcc1f4e5be799cfaa1e77
Roo <136781946+rociocarvajal@users.noreply.github.com>
2023-11-07 19:14:33 -0300

    Update trabajoLibre.php
    correccion trabajo

--- trabajoLibre.php
+++ trabajoLibre.php
@@ -1,112 +1,117 @@
 <?php
 
-# Trabajo Final Libre - Introducción a la Programación 
+ # Trabajo Final Libre - Introducción a la Programación 
 
-/**
- * Función que retorna array de comunicaciones de la red social.
+ /**
+ * Función que retorna array de datos de la red social.
  * Esta funcion carga y retorna algunos datos como: id, nick, cantLikes, cantComent
  * @return array 
  */
 
  function redSocial() 
  {
-    $comunicaciones[0] = array (
+    $datos[0] = array (
         "id" => "H1523",
         "nick" => "juan",
         "cantLikes" => 45,
         "cantComent" => 15
     );
-    $comunicaciones[1] = array (
+    $datos[1] = array (
         "id" => "P147",
         "nick" => "facu10",
         "cantLikes" => 12,
         "cantComent" => 65
     );
-    $comunicaciones[2] = array (
+    $datos[2] = array (
         "id" => "P7811",
         "nick" => "juan",
         "cantLikes" => 25,
         "cantComent" => 30
     );
-    $comunicaciones[3] = array (
+    $datos[3] = array (
         "id" => "H2587",
         "nick" => "marcia01",
         "cantLikes" => 36,
         "cantComent" => 0
     );
-    $comunicaciones[4] = array (
+    $datos[4] = array (
         "id" => "H1689",
         "nick" => "marcia01",
         "cantLikes" => 81,
         "cantComent" => 26
     );
-    return $comunicaciones;
+    return $datos;
  }
 
- /**
-  * Función que reciba por parametro un array asociativo y devuelva los datos de este mismo, clasificandolos por historia o publicación.
-  * @param array $comunicaciones
-  * @param int $i
-  */
+/**
+ * Función que reciba por parametro un array asociativo y devuelva los datos de este mismo, clasificandolos por historia o publicación.
+ * @param array $comunicacion
+ */
 
-  function mostrarData($comunicaciones, $i) {
-    $id = $comunicaciones[$i]["id"];
+    function mostrarData ($comunicacion) {
+    $id = $comunicacion['id'];
     $firstLetter = substr($id, 0, 1);
-    //substr() --> devuelve la primera letra de una cadena definida por los parametros start y length
-    if ($firstLetter == "H") {
+     //substr() --> devuelve la primera letra de una cadena definida por los parametros start y length
+     if ($firstLetter == "H") {
         $type = "Historia";
     }
     else {
         $type = "Publicacion";
     }
-    $autor = $comunicaciones[$i]["nick"];
-    $likes = $comunicaciones[$i]["cantLikes"];
-    $coment = $comunicaciones[$i]["cantComent"];
+    $autor = $comunicacion['nick'];
+    $likes = $comunicacion['cantLikes'];
+    $coment = $comunicacion['cantComent'];
     echo "- Comunicación: $id\n";
     echo "- Tipo: $type\n";
     echo "- Autor: $autor\n";
     echo "- Me gustas: $likes\n";
     echo "- Comentarios: $coment\n";
-  } 
+    }
 
- /**
+  /**
    * Función que segun un array, retorne el indice de la comunicación con mayor cantidad de likes.
-   * @param array $comunicaciones
-   * @return int
+   * @param array $datos
+   * @return int $newIndex
    */
 
-   function moreLikes ($comunicaciones) {
-    $n = count($comunicaciones);
-    $valorA = $comunicaciones[0]["cantLikes"];
+  function moreLikes ($datos) {
+    $n = count($datos);
+    $valorA = $datos[0]["cantLikes"];
+    $valorB = $datos[1]["cantLikes"];
     for ($i=0; $i < $n ; $i++) { 
-        if ($valorA < $comunicaciones[$i]["cantLikes"]) {
-            $valorA = $comunicaciones[$i]["cantLikes"];
+        if ($valorA < $datos[$i]["cantLikes"]) {
+            $valorA = $datos[$i]["cantLikes"];
+            $newIndex = $i;
+        }
+        elseif ($valorA > $valorB) {
+            $valorB = $valorA;
+            $valorA = $datos[$i]["cantLikes"];
             $newIndex = $i;
         }
     }
     return $newIndex;
    }
 
-   /**
+      /**
     * Funcion que segun un input de nickname, retorne un valor para determinar si es valido o evaluarlo segun condiciones.
-    * @param string $nickName
-    * @param array $comunicaciones
-    * @return int
+    * @param array $datos
+    * @param string $nickname
+    * @return int $n
     */
 
-    function validarNick ($nickName, $comunicaciones) {
+    function validarNickname ($datos, $nickname) {
         $n = 0;
-        for ($i=0; $i < count($comunicaciones); $i++) {
+        $i = 0;
+        $found = false;
+        while ($found == false && $i < count($datos)) {
             $newIndex = 0;
-            if ($comunicaciones[$i]["nick"] == $nickName) {
+            if ($datos[$i]["nick"] == $nickname) {
                 $newIndex = $newIndex + $i;
-                $nlikes = $comunicaciones[$newIndex]["cantLikes"];
-                $nComents = $comunicaciones[$newIndex]["cantComent"];
-                if ($nComents > $nlikes) {
-                    $n = $n + 100000;
-                    echo "Primer comunicacion de $nickName que los comentarios superan likes: \n";
-                    mostrarData($comunicaciones, $newIndex);
-                    break;
+                $nLikes = $datos[$newIndex]["cantLikes"];
+                $nComents = $datos[$newIndex]["cantComent"];
+                if ($nComents > $nLikes) {
+                    $n = $newIndex;
+                    $found = true;
                 }
                 else {
                     $n = -1;
@@ -115,43 +120,48 @@
             else {
                 $n = -2;
             }
+            $i++;
         }
         return $n;
     }
 
-   /**
+       /**
     * Función que encuentra a los mayores likes segun un número ingresado y retorna cantidad.
-    * @param array $comunicaciones
-    * @param int $number
-    * @return int
+    * @param array $datos
+    * @param int $inputLikes
+    * @return array $indexado
     */
 
-
- function maxLikes($comunicaciones, $number) {
-    $counter = 0;
-    for ($i=0; $i < count($comunicaciones); $i++) { 
-        if ($comunicaciones[$i]["cantLikes"] > $number) {
-            $counter = $counter + 1;
+    function maxLikes($datos, $inputLikes) {
+        $indexado = [];
+        $i = 0;
+        for ($i=0; $i < count($datos); $i++) { 
+            if ($datos[$i]["cantLikes"] > $inputLikes) {
+                $newArray[] = $datos[$i]["id"];
+                $indexado[] = $newArray;
+                
+            }
+            else {
+                $i++;
+            }
         }
-        else {
-            $counter = $counter;
-        }
+        return $indexado;
     }
-    return $counter;
- }
+
 
 /**
  * Función solicitar nueva comunicacion y agregarlo al array precargado.
- * @param array $comunicaciones
- * @return array
+ * @param array $datos
+ * @return array $datos
  */
 
- function add ($comunicaciones) {
+function add ($datos) {
     // Solicito datos al usuario
     echo "Ingrese id: ";
     $id = trim(fgets(STDIN));
     echo "Ingrese nickname: ";
     $nickname = trim(fgets(STDIN));
+    $nickname = strtolower($nickname);
     echo "Ingrese cantidad de likes: ";
     $nLikes = trim(fgets(STDIN));
     echo "Ingrese cantidad de comentarios: ";
@@ -163,19 +173,16 @@
         "cantLikes" => $nLikes,
         "cantComent" => $nComents
     );
-    $comunicaciones[] = $newDato;
-    return $comunicaciones;
+    $datos[] = $newDato;
+    return $datos;
  }
 
- /**
-  * Programa principal
-  */
+// Programa principal
 
-  $data = redSocial();
-  echo "Ingrese la cantidad de comunicaciones a agregar: ";
-  $cantComunicaciones = trim(fgets(STDIN));
-  //Recupero el array
-  if ($cantComunicaciones != 0) {
+$data = redSocial();
+echo "Ingrese cantidad de comunicaciones que desea agregar: ";
+$cantComunicaciones = trim(fgets(STDIN));
+if ($cantComunicaciones != 0) {
     for ($i=0; $i < $cantComunicaciones; $i++) { 
         $data = add($data);
       }
@@ -184,24 +191,29 @@
     echo "No se ingresaron comunicaciones.\n";
     $data = $data;
   }
-  $moreLikes = moreLikes($data);
-  echo "Datos de la comunicacion con más likes: \n";
-  mostrarData($data, $moreLikes);
-  echo "Ingrese nickname: ";
-  $nick = trim(fgets(STDIN));
-  $nick = strtolower($nick);
-  $result = validarNick($nick, $data);
-  if ($result == -2) {
-    echo "Nickname: $nick no encontrado en base de datos, intentar nuevamente.\n";
-  }
-  elseif ($result == -1) {
-    echo "Nickname: $nick existe, pero ninguna de sus comunicaciones tiene mayor cantidad de comentarios, que likes.\n";
-  }
-  echo "Ingrese cantidad de likes a superar: ";
-  $nLikes = trim(fgets(STDIN));
-  $moreLikes = maxLikes($data, $nLikes);
-  echo "La cantidad de comunicaciones que superan los likes ingresados: ($moreLikes).";
+$index = moreLikes($data);
+echo "La comunicacion con mas likes es: \n";
+$asociativo = $data[$index];
+mostrarData($asociativo);
+echo "Ingrese nickname: ";
+$nick = trim(fgets(STDIN));
+$nick = strtolower($nick);
+$number = validarNickname($data, $nick);
+if ($number >= 0) {
+    $newAsociativo = $data[$number];
+    echo "La primera comunicacion de $nick, que los comentarios superan a los likes es: \n";
+    print_r($newAsociativo);
+}
+elseif ($number == -1) {
+    echo "$nick existe en la base de datos, pero en ninguna de sus publicaciones los comentarios superan likes.\n";
+}
+else {
+    echo "$nick no existe en la base de datos.\n";
+}
+echo "Ingrese cantidad de likes a superar: ";
+$nLikes = trim(fgets(STDIN));
+$indexado = maxLikes($data, $nLikes);
+$cantidad = count($indexado);
+echo "La cantidad de comunicaciones que los likes superan input es un total de: $cantidad.\n";
 
-  ?>
-  
-
+?>
